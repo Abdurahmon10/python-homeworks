d96430302a95cbf419be83552abcd420c3878496
Abdurahmon <amuhammadjanov1010@gmail.com>
2025-02-06 15:20:50 +0500

    string changed

--- lesson-2/homework/string.py
+++ lesson-2/homework/string.py
@@ -1,95 +1,95 @@
-# #1
-# name=input()
-# by=int(input())
-# print(name,", your age is:",2025-by)
+#1
+name=input()
+by=int(input())
+print(name,", your age is:",2025-by)
 
-# #2
+#2
 
-# s = 'LMaasleitbtui'
+s = 'LMaasleitbtui'
 
-# print(s[::2])
-# print(s[1::2])
+print(s[::2])
+print(s[1::2])
 
-# #3
-# s=input()
-# print(s.len())
-# print(s.upper())
-# print(s.lower())
+#3
+s=input()
+print(s.len())
+print(s.upper())
+print(s.lower())
 
-# #4
-# s=input()
-# ss=s[::-1]
-# print(ss)
+#4
+s=input()
+ss=s[::-1]
+print(ss)
 
-# #5
-# s=input()
-# vovel=0
-# consonant=0
-# for i in s:
-#     if i in "aeiou":
-#         vovel+=1
-#     elif i in "qwrtypsdfghjklzxcvbnm":
-#         consonant+=1
-# print("vovel: ",vovel,"consonant :",consonant)
+#5
+s=input()
+vovel=0
+consonant=0
+for i in s:
+    if i in "aeiou":
+        vovel+=1
+    elif i in "qwrtypsdfghjklzxcvbnm":
+        consonant+=1
+print("vovel: ",vovel,"consonant :",consonant)
 
-# #6
-# s=input()
-# ss=input()
-# if(s in ss):
-#     print(s,"is in",ss)
-# if(ss in s):
-#     print(ss,"is in",s)
+#6
+s=input()
+ss=input()
+if(s in ss):
+    print(s,"is in",ss)
+if(ss in s):
+    print(ss,"is in",s)
 
-# #7
+#7
 
-# sentence=input()
-# replaced=input()
-# replacer=input()
+sentence=input()
+replaced=input()
+replacer=input()
 
-# sentence.replace(replaced,replacer)
+sentence.replace(replaced,replacer)
 
-# print(sentence)
+print(sentence)
 
-# #8
-# s=input()
-# print(s[0:0])
-# print(s[s.len():s.len()])
+#8
+s=input()
+print(s[0:0])
+print(s[s.len():s.len()])
 
-# #9
-# s=input()
-# print(s[::-1])
+#9
+s=input()
+print(s[::-1])
 
-# #10
-# s=input()
-# print(s.count(" ")+1)
+#10
+s=input()
+print(s.count(" ")+1)
 
-# #11
-# s=input()
-# print(any(1 for i in s if i in '1234567890'))
+#11
+s=input()
+print(any(1 for i in s if i in '1234567890'))
 
-# #12
-# print(','.join([input().split()]))
+#12
+print(','.join([input().split()]))
 
-# #13
-# print(input().replace(" ",""))
+#13
+print(input().replace(" ",""))
 
-# #14
-# s=input()
-# ss=input()
-# if(s==ss):
-#     print("they are equal.")
-# else:
-#     print("they are not equal.")
+#14
+s=input()
+ss=input()
+if(s==ss):
+    print("they are equal.")
+else:
+    print("they are not equal.")
 
-# #15
-# s="".join(i[0].upper() for i in [input().split()])
-# print(s)
+#15
+s="".join(i[0].upper() for i in [input().split()])
+print(s)
 
 
-# #16
-# s=input()
-# ch=(input())
-# print(s.replace(ch,""))
+#16
+s=input()
+ch=(input())
+print(s.replace(ch,""))
 
 #17
 s=input()
