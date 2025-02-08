878bbcf68701da6c575c7f2f73b1530a3d13873d
Abdurahmon <amuhammadjanov1010@gmail.com>
2025-02-08 20:33:23 +0500

    FIrst commit for lesson 3

---
+++ lesson-3/list-tasks.py
@@ -0,0 +1,10 @@
+#1
+a=list(input("Please enter the list").split(" "))
+b=input()
+print(b,"is repeated",a.count(b),"times inside the list")
+
+#2
+a=list(map(int,input("PLease enter the list:\n").split(" ")))
+print("Sum of all elements:",sum(a))
+
+#3
